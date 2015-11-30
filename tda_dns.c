063208cc01678541927d896a6284a925d89ded83
joakoracing <joakoracing@hotmail.com>
2015-11-30 16:36:05 -0300

    Update tda_dns.c

--- tda_dns.c
+++ tda_dns.c
@@ -1,5 +1,20 @@
 #include "dns.h"
 
+int AB_Busqueda(tdns dns, const int mov, char* domain){     /*ver el tipo del movimiento*/
+    int err, cmp, *error;
+    tdomain Aux;
+    err=AB_MoverCte(dns->ab, mov, error);
+    if(!err)
+        return NOT_FOUNDED;                                     
+    AB_ElemCte(dns->ab,&Aux);
+    if(doamin==Aux.domain)
+        return FOUNDED;                                    /*DEFINE*/
+    if(AB_CanMove(dns->ab,DER))
+        return AB_Busqueda(dns, DER, domain);
+    if(AB_CanMove(dns->ab,IZQ))                            /*VER SI ESTA BIEN HACER UNA PRIMITIVA PARA ESTO EN TDA AB*/
+        return AB_Busqueda(dns, IZQ, domain);
+}
+
 int createDNS(tdns *dns, int dataSize) {
     dns->ab = (TAB) malloc(sizeof(TAB));
     if (!(dns->ab))
@@ -15,23 +30,17 @@ void destroyDNS(tdns *dns) {
     free(dns);
 }
 
-int orderInsert(tdns *dns, tdomain *domain);
-
-void loadTree(tdns *dns, char *configFile) {
-    FILE *cfile;
-    tdomain temp;
-    char *line[MAX_LINE];
-
-    cfile = fopen(configFile, "r");
-
-    while(!feof(cfile)) {
-        if (fgets(linea, MAX_LINE-1, cfile)) {
-            temp.domain = strtok(line, " ");
-            tmep.ip = strtok(NULL, "");
-            if ((!temp.domain) || (!temp.ip))
-                return 0;
-            orderInsert(dns, temp);
-        }
-    }
-    fclose(cfile);
+void getValue(tdns dns, char* domain, void* data){
+    tdomain Aux;
+    if(AB_Vacio(dns->ab))
+        return NO_DATA;              /*DEFINE*/
+    AB_ElemCte(dns->ab,&Aux);
+    domain=Aux.domain;
+    (char*)data=Aux.ip;             /*VER SI SE PUEDE CASTEAR ARGUMENTO Y EL TIPO DE AUX:IP*/
+    return RES_OK;
 }
+
+int domainExists(tdns dns, char* domain){
+    return AB_Busqueda(dns, raiz, domain);
+}
+
