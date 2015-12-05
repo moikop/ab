ead4c2b4279b03eea09a62abaff70b0cb5b46944
nachox86 <nachox86@users.noreply.github.com>
2015-12-05 16:46:51 -0300

    agrego funcion de validacion de entrada
    función de validacion de los parámetros de entrada al programa

--- main.c
+++ main.c
@@ -1,43 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "tda_dns.h"
+
+#define ARGS_DNS_SEND 6
+#define ARGS_DNS_GET_IP 5
+#define ARGS_DNS_ADD_DOMAIN 5
+#define ARGS_DNS_DELETE_DOMAIN 4
 #define CMD_SEND    "-dnsSend"
 #define CMD_GETIP   "-dnsGetIP"
 #define CMD_ADDDOMAIN   "-dnsAddDomain"
 #define CMD_DELETEDOMAIN    "-dnsDeleteDomain"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char** argv) {
     FILE *logf;
 
-    if (strcmp(argv[1], CMD_SEND) == 0) {
-        /*
-        argv[2] == srcurl
-        argv[3] == dsturl
-        argv[4] == msg
-        argv[5] == logger file
-        */
-    } else if (strcmp(argv[1], CMD_GETIP) == 0) {
-        /*
-        argv[2] == srcurl
-        argv[3] == dsturl
-        argv[4] == logger file
-        */
-    } else if (strcmp(argv[1], CMD_ADDDOMAIN) == 0) {
-        /*
-        argv[2] == url
-        argv[3] == IP
-        argv[4] == logger file
-        */
-    } else if (strcmp(argv[1], CMD_DELETEDOMAIN) == 0) {
-        /*
-        argv[2] == url
-        argv[3] == logger file
-        */
-    } else {
-        /*
-        return ERROR_UNKNOWN_CMD
-        */
+    char cmd[100];
+
+    if(validateInput(argv,&cmd)!=RES_OK) {
+        showHelp();
+        return RES_ERROR;
     }
 
-    return 0;
+
+
+    return RES_OK;
 }
