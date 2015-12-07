35d0f9fbde5bb3f395aa059f1875a0838d53d768
nachox86 <nachox86@users.noreply.github.com>
2015-12-07 20:24:34 -0300

    agrego switch para lo q pide el usuario

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "tda_dns.h"
+#include "tda_dns.c"
 
 #define ARGS_DNS_SEND 6
 #define ARGS_DNS_GET_IP 5
@@ -14,15 +15,39 @@
 
 int main(int argc, char** argv) {
     FILE *logf;
-
+    FILE* dns_file;
     char cmd[100];
+    char file_dns[100] = "dns.txt";
+    char line[300];
+    tdns dns;
+    int loaded;
 
+    /*validacion parámetros de entrada*/
     if(validateInput(argv,&cmd)!=RES_OK) {
         showHelp();
         return RES_ERROR;
     }
 
+    createDNS(&dns,sizeof(tdomain));
 
-    free(cmd);
+    /*procesamos el archivo con las direcciones y dominios; en el proceso */
+    loaded = loadTree(&dns,file_dns);
+    if(loaded!=RES_OK) return loaded;
+
+    /*setear una estructura con los datos que se pasan*/
+
+    /*Decidimos que accion es la que nos pide el usuario*/
+
+    switch(argv[1]) {
+        case CMD_SEND: break;
+        case CMD_GETIP: break;
+        case CMD_ADDDOMAIN:
+            /*setear una estructura con los datos que se pasan*/
+            if(addDomain(&dns,argv[2],const tdomain* td)!=RES_OK) return RES_ERROR;
+            break;
+        case CMD_DELETEDOMAIN: break;
+        default:
+    }
+
     return RES_OK;
 }
