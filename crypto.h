60977e83549ef34333f2c97fb5f4bb3c52766ea1
nachox86 <nachox86@users.noreply.github.com>
2015-12-08 12:49:36 -0300

    corrijo unas cosas
    corrijo unas cosas ,  ahora anda bien

--- crypto.h
+++ crypto.h
@@ -5,19 +5,26 @@
 
 #define CRYPTO_DIR "./crypto/"
 #define MAX_OFFSET 20
+#define MAX_LINE 256
 
-void encryptMsg(char *msg, char offset) {
+#define RES_OK 0
+#define RES_ERROR 1
+#define RES_MEM_ERROR -1
+
+int encryptMsg(char *msg, char offset) {
     int i;
 
     for (i = 0; i < strlen(msg); i++)
         msg[i] += offset;
+    return RES_OK;
 }
 
-void decryptMsg(char *msg, char offset) {
+int decryptMsg(char *msg, char offset) {
     int i;
 
     for (i = 0; i < strlen(msg); i++)
         msg[i] -= offset;
+    return RES_OK;
 }
 
 int genoffset(char *domain) {
@@ -25,42 +32,45 @@ int genoffset(char *domain) {
     char *fpath = NULL;
     char offset = 0;
 
-    fpath = malloc(sizeof(char) * (strlen(domain) + strlen(CRYPTO_DIR)));
-    if (!fpath) return 0;
+    fpath = malloc(sizeof(char) * (strlen(domain) + strlen(CRYPTO_DIR))+1);
+    if (!fpath) return RES_MEM_ERROR;
 
     strcpy(fpath, CRYPTO_DIR);
     strcat(fpath, domain);
 
     crypto = fopen(fpath, "w");
-    if (!crypto) return 0;
+    if (!crypto) return RES_ERROR;
 
     srand(time(NULL));
     offset = (rand() % MAX_OFFSET) + 1;
-
+    printf("el offset será: %c\n",offset);
     fprintf(crypto, "%c", offset);
     fclose(crypto);
 
     free(fpath);
-    return 1;
+    return RES_OK;
 }
 
-char getoffset(char *domain) {
+int getoffset(char *domain,char* offset) {
     FILE *crypto;
     char *fpath = NULL;
     char line[5];
 
-    fpath = malloc(sizeof(char) * (strlen(domain) + strlen(CRYPTO_DIR)));
-    if (!fpath) return 0;
+    fpath = malloc(sizeof(char)*(strlen(domain)+strlen(CRYPTO_DIR))+1);
+    if (!fpath) return RES_MEM_ERROR;
 
     strcpy(fpath, CRYPTO_DIR);
     strcat(fpath, domain);
 
     crypto = fopen(fpath, "r");
-    if (!crypto) return 0;
+    if (!crypto) return RES_ERROR;
 
-    if (!feof(crypto))
-        if (fgets(line, 5, crypto))
-            return *line;
-
-    return 0;
+    if (!feof(crypto)){
+        if (fgets(line, 5, crypto)) {
+            strcpy(offset,line);
+            return RES_OK;
+        }
+    }
+    offset = '\0';
+    return RES_ERROR;
 }
